6ad3a37317deeb112b253963edcb24f81e63daef
dibyendu-mahata <dibyendumahata2016@gmail.com>
2023-08-14 12:59:06 +0530

    C1

---
+++ app/src/main/java/com/androidactivity/splashscreen_github/ReadMe.kt
@@ -0,0 +1,11 @@
+package com.androidactivity.splashscreen_github
+
+/*
+* Steps
+* 1. Add Dependency & Icon (Drawable)
+* 2. Create a separate file for Splash Screen (It is not mandatory) -> res/values/splash.xml
+* 3. Go to Manifest, replace Default Theme with Splash Theme
+* 4. Resize the Icon, if necessary (most of the time it is necessary)
+* 5. "installSplashScreen" at onCreate
+* 6. Add delay if necessary
+*/
